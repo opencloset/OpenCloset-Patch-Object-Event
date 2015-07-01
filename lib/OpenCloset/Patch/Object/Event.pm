b729d42e10d655d064d5a0e22cd771d34e7e9135
Keedi Kim <keedi.k@gmail.com>
2015-07-01 17:40:08 +0900

    Ready to dzil

---
+++ lib/OpenCloset/Patch/Object/Event.pm
@@ -0,0 +1,24 @@
+package OpenCloset::Patch::Object::Event;
+# ABSTRACT: OpenCloset Object::Event Patch
+
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=head1 SYNOPSIS
+
+    use OpenCloset::Patch::Object::Event;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
